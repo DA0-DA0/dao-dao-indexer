a528fba5bf130091090873ec62b790ba08ce9cd5
Noah Saso <noahsaso@gmail.com>
2022-12-02 20:04:11 -0800

    No need to validate relationship between value and delete. Sometimes value is an empty string even when delete is true. Fine.

--- src/db/models/Event.ts
+++ src/db/models/Event.ts
@@ -12,16 +12,6 @@ import { Contract } from './Contract'
 
 @Table({
   timestamps: true,
-  validate: {
-    valueIsNullIffDelete() {
-      if (this.delete && this.value !== null) {
-        throw new Error('value must be null when delete is true')
-      }
-      if (!this.delete && this.value === null) {
-        throw new Error('value must not be null when delete is false')
-      }
-    },
-  },
   indexes: [
     // Only one event can happen to a key for a given contract at a given block
     // height. This ensures events are not duplicated if they attempt exporting
