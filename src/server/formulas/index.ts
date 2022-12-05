7af03014dc3dd564fba0d773d63ba68ce904ed3e
Noah Saso <noahsaso@gmail.com>
2022-12-04 21:50:25 -0800

    Added cw20 to formulas registry.

--- src/server/formulas/index.ts
+++ src/server/formulas/index.ts
@@ -1,10 +1,12 @@
 import { Formula } from '../types'
 import * as common from './common'
+import * as cw20 from './cw20'
 import * as daoCore from './daoCore'
 import * as daoProposalSingle from './daoProposalSingle'
 
 export const formulas = {
   ...common,
+  cw20,
   daoCore,
   daoProposalSingle,
 }
