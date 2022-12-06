9c56dc1c2cb216036cd9fd0a2a297845165f82f3
Noah Saso <noahsaso@gmail.com>
2022-12-05 21:02:12 -0800

    Formatted daoPreProposeSingle/depositInfo as expected.

--- src/server/formulas/daoPreProposeSingle.ts
+++ src/server/formulas/daoPreProposeSingle.ts
@@ -8,5 +8,19 @@ export const config: Formula<any | undefined> = async ({
 export const depositInfo: Formula<
   any | undefined,
   { proposalId: string }
-> = async ({ contractAddress, get, args: { proposalId } }) =>
-  await get(contractAddress, 'deposits', Number(proposalId))
+> = async ({ contractAddress, get, args: { proposalId } }) => {
+  const data = await get<[any, string]>(
+    contractAddress,
+    'deposits',
+    Number(proposalId)
+  )
+
+  if (!data || !Array.isArray(data) || data.length !== 2) {
+    return
+  }
+
+  return {
+    deposit_info: data[0],
+    proposer: data[1],
+  }
+}
