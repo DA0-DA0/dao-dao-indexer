4dd20b9302e6757083c70f027c4d22d0b7bb942b
Noah Saso <noahsaso@gmail.com>
2022-12-19 10:31:58 -0800

    Added cw20 token info to wasmswap price query.

--- src/core/formulas/external/wasmswap.ts
+++ src/core/formulas/external/wasmswap.ts
@@ -1,12 +1,15 @@
 import { Formula } from '../../types'
 import { Denom } from '../types'
+import { tokenInfo } from './cw20'
 
 interface Token {
   reserve: string
   denom: Denom
 }
 
-export const price: Formula = async ({ contractAddress, get }) => {
+export const price: Formula = async (env) => {
+  const { contractAddress, get } = env
+
   const token1 = await get<Token>(contractAddress, 'token1')
   const token2 = await get<Token>(contractAddress, 'token2')
   if (!token1 || !token2) {
@@ -20,14 +23,26 @@ export const price: Formula = async ({ contractAddress, get }) => {
     return undefined
   }
 
+  // Get CW20 token info if available.
+  const token1Cw20 =
+    'cw20' in token1.denom
+      ? await tokenInfo({ ...env, contractAddress: token1.denom.cw20 })
+      : undefined
+  const token2Cw20 =
+    'cw20' in token2.denom
+      ? await tokenInfo({ ...env, contractAddress: token2.denom.cw20 })
+      : undefined
+
   return {
     token1: {
       denom: token1.denom,
-      price: (token2Amount / token1Amount).toString(),
+      cw20Info: token1Cw20,
+      price: token2Amount / token1Amount,
     },
     token2: {
       denom: token2.denom,
-      price: (token1Amount / token2Amount).toString(),
+      cw20Info: token2Cw20,
+      price: token1Amount / token2Amount,
     },
   }
 }
