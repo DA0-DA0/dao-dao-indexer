8bc1be06865fa1cd92631ebcb297c7610d76cce0
Noah Saso <noahsaso@gmail.com>
2023-02-05 02:33:16 -0800

    Fixed cw-receipt webhook secret.

--- src/data/webhooks/indexerCwReceipt.ts
+++ src/data/webhooks/indexerCwReceipt.ts
@@ -5,7 +5,7 @@ const KEY_PREFIX_RECEIPT_TOTALS = dbKeyForKeys('receipt_totals', '')
 
 // Fire webhook when a payment is paid to the indexer's cw-receipt contract.
 export const makeIndexerCwReceiptPaid: WebhookMaker = (config) =>
-  !config.payment?.cwReceiptAddress
+  !config.payment
     ? null
     : {
         filter: {
@@ -13,13 +13,16 @@ export const makeIndexerCwReceiptPaid: WebhookMaker = (config) =>
           // Filter for receipt_totals state changes.
           matches: (event) => event.key.startsWith(KEY_PREFIX_RECEIPT_TOTALS),
         },
-        endpoint: async () => ({
-          url: 'https://indexer-mainnet.daodao.zone/account/webhook/cw-receipt',
-          method: 'POST',
-          headers: {
-            'X-API-Key': config.discordNotifierApiKey,
-          },
-        }),
+        endpoint: async () =>
+          !config.payment
+            ? undefined
+            : {
+                url: 'https://indexer-mainnet.daodao.zone/account/webhook/cw-receipt',
+                method: 'POST',
+                headers: {
+                  'X-API-Key': config.payment.cwReceiptWebhookSecret,
+                },
+              },
         getValue: async (event) => {
           // "receipt_totals" | receiptId | serializedDenom
           const [, receiptId, serializedDenom] = dbKeyToKeys(event.key, [
