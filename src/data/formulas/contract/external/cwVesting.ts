0a680ff569d1dcebb8e10376ade81290657735f4
Noah Saso <noahsaso@gmail.com>
2023-03-06 11:35:16 -0800

    Added validators map formula to cw-vesting.

--- src/data/formulas/contract/external/cwVesting.ts
+++ src/data/formulas/contract/external/cwVesting.ts
@@ -4,3 +4,11 @@ export const info: ContractFormula = {
   compute: async ({ contractAddress, get }) =>
     await get(contractAddress, 'vesting_payment'),
 }
+
+export const validators: ContractFormula = {
+  dynamic: true,
+  compute: async ({ contractAddress, getMap }) => {
+    const validators = await getMap(contractAddress, 'validator')
+    return validators
+  },
+}
