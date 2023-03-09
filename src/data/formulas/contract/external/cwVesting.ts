0e304eacf8e997801ea76d5c48cf0c58afa519f1
Noah Saso <noahsaso@gmail.com>
2023-03-08 18:02:55 -0800

    Added query to cw-vesting to get validator amounts by time.

--- src/data/formulas/contract/external/cwVesting.ts
+++ src/data/formulas/contract/external/cwVesting.ts
@@ -1,14 +1,39 @@
-import { ContractFormula } from '@/core'
+import { ContractFormula, dbKeyToKeys } from '@/core'
+
+type Validator = {
+  validator: string
+  timeMs: number
+  amount: string
+}
 
 export const info: ContractFormula = {
   compute: async ({ contractAddress, get }) =>
     await get(contractAddress, 'vesting_payment'),
 }
 
-export const validators: ContractFormula = {
-  dynamic: true,
+export const validators: ContractFormula<Validator[]> = {
   compute: async ({ contractAddress, getMap }) => {
-    const validators = await getMap(contractAddress, 'validator')
+    const validatorsMap =
+      (await getMap(contractAddress, 'validator', {
+        keyType: 'raw',
+      })) ?? {}
+
+    const validators = Object.entries(validatorsMap)
+      .map(([key, amount]): Validator => {
+        const [validator, epoch] = dbKeyToKeys(key, [false, true]) as [
+          string,
+          number
+        ]
+
+        return {
+          validator,
+          timeMs: epoch * 1000,
+          amount,
+        }
+      })
+      // Sort ascending by time.
+      .sort((a, b) => a.timeMs - b.timeMs)
+
     return validators
   },
 }
