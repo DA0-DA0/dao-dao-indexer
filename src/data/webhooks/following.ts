c5e86685918a369d0effbc11d8b4f8bcb506227d
Noah Saso <noahsaso@gmail.com>
2023-04-24 00:18:38 -0700

    Support following DAO notifications for NFT and native staking DAOs.

--- src/data/webhooks/following.ts
+++ src/data/webhooks/following.ts
@@ -3,17 +3,29 @@ import { dbKeyForKeys, dbKeyToKeys } from '@/core/utils'
 
 const CW20_STAKE_CODE_IDS_KEY = 'cw20-stake'
 const DAO_VOTING_CW4_CODE_IDS_KEY = 'dao-voting-cw4'
+const DAO_VOTING_CW721_STAKED_CODE_IDS_KEY = 'dao-voting-cw721-staked'
+const DAO_VOTING_NATIVE_STAKED_CODE_IDS_KEY = 'dao-voting-native-staked'
 
+// cw20-stake and dao-voting-native-staked
 const KEY_PREFIX_STAKED_BALANCES = dbKeyForKeys('staked_balances', '')
+// dao-voting-cw4
 const KEY_PREFIX_USER_WEIGHTS = dbKeyForKeys('user_weights', '')
+// dao-voting-cw721-staked
+const KEY_PREFIX_NB = dbKeyForKeys('nb', '')
 
-// Fire webhook when a cw20-stake balance or dao-voting-cw4 user weight is set.
+// Fire webhook when a user becomes a member of a DAO.
 export const makeAddPendingFollow: WebhookMaker = (config, state) => ({
   filter: {
-    codeIdsKeys: [CW20_STAKE_CODE_IDS_KEY, DAO_VOTING_CW4_CODE_IDS_KEY],
+    codeIdsKeys: [
+      CW20_STAKE_CODE_IDS_KEY,
+      DAO_VOTING_CW4_CODE_IDS_KEY,
+      DAO_VOTING_CW721_STAKED_CODE_IDS_KEY,
+      DAO_VOTING_NATIVE_STAKED_CODE_IDS_KEY,
+    ],
     matches: (event) =>
       (event.key.startsWith(KEY_PREFIX_STAKED_BALANCES) ||
-        event.key.startsWith(KEY_PREFIX_USER_WEIGHTS)) &&
+        event.key.startsWith(KEY_PREFIX_USER_WEIGHTS) ||
+        event.key.startsWith(KEY_PREFIX_NB)) &&
       !event.delete &&
       event.valueJson !== '0',
   },
@@ -35,6 +47,7 @@ export const makeAddPendingFollow: WebhookMaker = (config, state) => ({
           event.contractAddress
         )
       )?.contractAddress
+
       if (daoVotingCw20StakedAddress) {
         // Get DAO from voting contract.
         const dao = (
@@ -53,6 +66,20 @@ export const makeAddPendingFollow: WebhookMaker = (config, state) => ({
       // Get DAO from voting contract.
       const dao = (
         await env.getTransformationMatch(event.contractAddress, 'daoAddress')
+      )?.value
+
+      daoAddress = typeof dao === 'string' && !!dao ? dao : undefined
+    }
+    // If dao-voting-cw721-staked or dao-voting-native-staked...
+    else if (
+      [
+        ...env.getCodeIdsForKeys(DAO_VOTING_CW721_STAKED_CODE_IDS_KEY),
+        ...env.getCodeIdsForKeys(DAO_VOTING_NATIVE_STAKED_CODE_IDS_KEY),
+      ].includes(event.contract.codeId)
+    ) {
+      // Get DAO from voting contract.
+      const dao = (
+        await env.getTransformationMatch(event.contractAddress, 'dao')
       )?.value
 
       daoAddress = typeof dao === 'string' && !!dao ? dao : undefined
