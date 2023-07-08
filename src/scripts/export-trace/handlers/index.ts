59768c058c0142be3d71e706b2e6527445becb2f
Noah Saso <noahsaso@gmail.com>
2023-07-08 23:59:49 +0200

    Use trace pipe instead of rotating disk file.

--- src/scripts/export-trace/handlers/index.ts
+++ src/scripts/export-trace/handlers/index.ts
@@ -4,4 +4,6 @@ import { wasm } from './wasm'
 // Lines will be handled in the order of this array, so earlier handlers take
 // precedence. If an earlier handler returns true, the line will not be passed
 // to later handlers.
-export const handlerMakers: HandlerMaker[] = [wasm]
+export const handlerMakers: Record<string, HandlerMaker> = {
+  wasm,
+}
--- src/scripts/export-trace/index.ts
+++ src/scripts/export-trace/index.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs'
-import readline from 'readline'
+import * as net from 'net'
 
 import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate'
 import * as Sentry from '@sentry/node'
 import { Command } from 'commander'
+import { PromiseSocket } from 'promise-socket'
 
 import { DbType, loadConfig, objectMatchesStructure } from '@/core'
 import { State, loadDb } from '@/db'
@@ -19,15 +20,16 @@ program.option(
   'path to config file, falling back to config.json'
 )
 program.option(
-  '-i, --initial <block height>',
-  'block height to start exporting from, falling back to after the last exported block',
-  (value) => BigInt(value)
-)
-program.option(
   '-b, --batch <size>',
   'batch size',
   (value) => parseInt(value, 10),
   1000
+)
+program.option(
+  '-f, --flush <seconds>',
+  'flush at least every n seconds',
+  (value) => parseInt(value, 10),
+  2
 )
 program.option(
   // Adds inverted `update` boolean to the options object.
@@ -47,8 +49,8 @@ program.option(
 program.parse()
 const {
   config: _config,
-  initial,
   batch,
+  flush,
   update,
   webhooks,
   modules: _modules,
@@ -62,15 +64,6 @@ if (config.sentryDsn) {
   Sentry.init({
     dsn: config.sentryDsn,
   })
-}
-
-let reading = false
-// When true, shut down ASAP.
-let shuttingDown = false
-
-const exit = () => {
-  setReadingFile(false)
-  process.exit(0)
 }
 
 const main = async () => {
@@ -91,7 +84,7 @@ const main = async () => {
 
   // Ensure trace file exists.
   if (!config.trace || !fs.existsSync(config.trace)) {
-    fs.closeSync(fs.openSync(config.trace, 'w'))
+    throw new Error(`Trace file not found: ${config.trace}`)
   }
 
   let cosmWasmClient: CosmWasmClient | undefined
@@ -112,33 +105,14 @@ const main = async () => {
     process.send('ready')
   }
 
-  return Promise.all([
-    // Update state every second.
-    new Promise(() => {
-      // Update db state every second.
-      const stateInterval = setInterval(async () => {
-        await updateState(cosmWasmClient!, altCosmWasmClient)
+  // Update db state every second.
+  setInterval(
+    () => updateState(cosmWasmClient!, altCosmWasmClient),
+    1000
+  ).unref()
 
-        if (shuttingDown) {
-          clearInterval(stateInterval)
-        }
-      }, 1000)
-    }),
-    // Start reading.
-    reader(cosmWasmClient, altCosmWasmClient),
-  ])
-}
-
-// Create or remove trace reading file.
-const setReadingFile = (reading: boolean) => {
-  const readingFile = config.trace + '.reading'
-  if (reading) {
-    fs.closeSync(fs.openSync(readingFile, 'w'))
-  } else {
-    if (fs.existsSync(readingFile)) {
-      fs.unlinkSync(readingFile)
-    }
-  }
+  // Read from trace file.
+  await trace(cosmWasmClient, altCosmWasmClient)
 }
 
 let lastBlockHeight = 0
@@ -209,232 +183,223 @@ const updateState = async (
   return state
 }
 
-const reader = (
+let fd: number | undefined
+
+const trace = async (
   cosmWasmClient: CosmWasmClient,
   altCosmWasmClient: CosmWasmClient
-) =>
-  new Promise(async (_, reject) => {
-    // Update state with latest from RPC.
-    const initialState = await updateState(cosmWasmClient, altCosmWasmClient)
+) => {
+  // Update state with latest from RPC.
+  await updateState(cosmWasmClient, altCosmWasmClient)
 
-    // Setup handlers.
-    const handlers = handlerMakers.map((handlerMaker) =>
-      handlerMaker({
+  // Setup handlers.
+  const handlers = Object.entries(handlerMakers).map(
+    ([name, handlerMaker]) => ({
+      name,
+      handler: handlerMaker({
         cosmWasmClient,
         altCosmWasmClient,
         config,
         batch,
         updateComputations: !!update,
         sendWebhooks: !!webhooks,
-      })
-    )
+      }),
+    })
+  )
 
-    const initialBlock =
-      initial !== undefined
-        ? initial
-        : // Start at the next block after the last exported block if no initial
-          // block set.
-          BigInt(initialState.lastWasmBlockHeightExported ?? '0') + 1n
+  fd = fs.openSync(
+    config.trace,
+    fs.constants.O_RDONLY | fs.constants.O_NONBLOCK
+  )
+  const pipe = new PromiseSocket(
+    new net.Socket({
+      fd,
+    })
+  )
+  pipe.setEncoding('utf-8')
 
-    console.log(
-      `Catching up to initial block ${initialBlock.toLocaleString()}...`
-    )
+  // Flush all handlers.
+  const flushAll = async () => {
+    let tries = 3
+    while (tries > 0) {
+      for (const { handler } of handlers) {
+        try {
+          await handler.flush()
+          break
+        } catch (err) {
+          tries--
 
-    let catchingUp = true
-
-    // Read state per module.
-    let pendingRead = false
-    let bytesRead = 0
-
-    // Main logic. Read through each module.
-    const read = async () => {
-      if (shuttingDown) {
-        return
-      }
-
-      try {
-        reading = true
-        setReadingFile(true)
-
-        const fileStream = fs.createReadStream(config.trace, {
-          start: bytesRead,
-        })
-        const rl = readline.createInterface({
-          input: fileStream,
-          // Recognize all instances of CR LF ('\r\n') as a single line break.
-          crlfDelay: Infinity,
-          terminal: false,
-        })
-
-        // Buffer lines until we have a full JSON object.
-        let bufferedLine = ''
-        let noBufferReset = false
-
-        for await (const line of rl) {
-          // Reset buffer if we read a line without a full JSON object.
-          if (!noBufferReset) {
-            bufferedLine = ''
-            noBufferReset = false
-          }
-
-          if (shuttingDown) {
-            exit()
-            return
-          }
-
-          if (!line) {
-            noBufferReset = false
-            continue
-          }
-
-          bufferedLine += line
-          // Validate JSON. If invalid, buffer and wait for next line. This is
-          // necessary if we start reading a line before it is fully written.
-          // The first pass will read the first part of the line, and the second
-          // pass will read the rest of the line.
-          let tracedEvent: TracedEvent | undefined
-          try {
-            tracedEvent = JSON.parse(bufferedLine)
-          } catch (err) {
-            noBufferReset = true
-            console.log(
-              'Partial line read, buffering...',
-              err,
-              bufferedLine,
-              line
-            )
-            await new Promise((resolve) => setTimeout(resolve, 2000))
-            continue
-          }
-
-          // Ensure this is a traced write event.
-          if (
-            !tracedEvent ||
-            !objectMatchesStructure(tracedEvent, {
-              operation: {},
-              key: {},
-              value: {},
-              metadata: {
-                blockHeight: {},
-                txHash: {},
+          if (tries > 0) {
+            console.error(`Failed to flush. Trying ${tries} more time(s)...`)
+          } else {
+            console.error('Failed to flush. Giving up.')
+            Sentry.captureException(err, {
+              tags: {
+                type: 'failed-flush',
+                script: 'export-trace',
               },
-            }) ||
-            (tracedEvent.operation !== 'write' &&
-              tracedEvent.operation !== 'delete')
-          ) {
-            continue
+            })
           }
-
-          // If trace is from a block before the initial block, skip.
-          if (BigInt(tracedEvent.metadata.blockHeight) < initialBlock) {
-            continue
-          } else if (catchingUp) {
-            console.log(
-              `Caught up to initial block ${initialBlock.toLocaleString()}.`
-            )
-            catchingUp = false
-          }
-
-          // Try to handle each line with each module, and stop once handled.
-          for (const handler of handlers) {
-            try {
-              const handled = await handler.handle(tracedEvent)
-              if (handled) {
-                break
-              }
-            } catch (err) {
-              console.error('Failed to handle event', err)
-              // Restart the read without flushing or updating bytes read.
-              read()
-              return
-            }
-          }
-        }
-
-        // Tell each handler to flush once we finish reading.
-        for (const handler of handlers) {
-          try {
-            await handler.flush()
-          } catch (err) {
-            console.error('Failed to flush', err)
-            // Restart the read without updating bytes read.
-            read()
-            return
-          }
-        }
-
-        // Update bytes read so we can start at this point in the next read.
-        bytesRead += fileStream.bytesRead
-
-        // Stop reading if shutting down.
-        if (shuttingDown) {
-          reading = false
-          return
-        }
-
-        // Read again if pending.
-        if (pendingRead) {
-          console.log('Finished reading and continuing due to more available.')
-          pendingRead = false
-          read()
-        } else {
-          reading = false
-          console.log('Finished reading trace file.')
-        }
-      } catch (err) {
-        reading = false
-        Sentry.captureException(err, {
-          tags: {
-            script: 'export-trace',
-          },
-          extra: {
-            bytesRead,
-          },
-        })
-        reject(err)
-      } finally {
-        if (!reading) {
-          setReadingFile(false)
         }
       }
     }
+  }
 
-    // Watch file for changes every half a second and read when more data is
-    // available.
-    const file = fs.watchFile(config.trace, { interval: 500 }, (curr, prev) => {
-      if (shuttingDown) {
-        return
+  // Flush every n seconds.
+  setInterval(flushAll, flush * 1000).unref()
+
+  let buffer = ''
+  for await (const chunk of pipe) {
+    // setEncoding('utf-8') will return strings, type-check to be safe.
+    if (typeof chunk !== 'string') {
+      continue
+    }
+
+    const lines = chunk.split('\n')
+    for (const line of lines) {
+      if (!line) {
+        continue
       }
 
-      // If modified, read if not already reading, and store that there is
-      // data to read otherwise.
-      if (curr.mtime > prev.mtime) {
-        if (!reading) {
-          read()
-        } else {
-          pendingRead = true
+      // Only begin buffer with a JSON object.
+      if (!buffer && !line.startsWith('{')) {
+        throw new Error(`Invalid line beginning: ${line}`)
+      }
+
+      buffer += line
+
+      // Check if buffer is a valid JSON object, and process if so.
+      let tracedEvent: TracedEvent | undefined
+      try {
+        tracedEvent = JSON.parse(buffer)
+      } catch (err) {
+        // If invalid but begins correctly, continue buffering.
+        if (
+          err instanceof SyntaxError &&
+          err.message.includes('Unexpected end of JSON input')
+        ) {
+          console.log(
+            'JSON incomplete from buffer. Buffering...',
+            JSON.stringify(
+              {
+                buffer,
+                line,
+              },
+              null,
+              2
+            )
+          )
+          continue
         }
-      }
-    })
-    file.on('error', reject)
 
-    // Start reading.
-    read()
-  })
+        // Capture other unexpected errors and ignore.
+        console.error(
+          'Failed to parse JSON',
+          err,
+          JSON.stringify(
+            {
+              chunk,
+              buffer,
+              line,
+            },
+            null,
+            2
+          )
+        )
+        Sentry.captureException(err, {
+          tags: {
+            type: 'ignored-trace',
+            script: 'export-trace',
+          },
+          extra: {
+            chunk,
+            buffer,
+            line,
+          },
+        })
+
+        // Reset buffer after processing and ignore.
+        buffer = ''
+        continue
+      }
+
+      try {
+        // Ensure this is a traced write event. Otherwise, reset buffer.
+        if (
+          !tracedEvent ||
+          !objectMatchesStructure(tracedEvent, {
+            operation: {},
+            key: {},
+            value: {},
+            metadata: {
+              blockHeight: {},
+              txHash: {},
+            },
+          }) ||
+          (tracedEvent.operation !== 'write' &&
+            tracedEvent.operation !== 'delete')
+        ) {
+          continue
+        }
+
+        // Try to handle with each module, and stop once handled.
+        for (const { name, handler } of handlers) {
+          let tries = 3
+          let handled = false
+          while (tries > 0) {
+            try {
+              handled = await handler.handle(tracedEvent)
+              // Set tries to 0 to break out of retry loop.
+              tries = 0
+            } catch (err) {
+              tries--
+
+              if (tries > 0) {
+                console.error(
+                  `[${name}] Failed to handle. Trying ${tries} more time(s)...`
+                )
+              } else {
+                console.error(`[${name}] Failed to handle. Giving up.`)
+                Sentry.captureException(err, {
+                  tags: {
+                    type: 'failed-handle',
+                    script: 'export-trace',
+                  },
+                  extra: {
+                    handler: name,
+                    tracedEvent,
+                  },
+                })
+              }
+            }
+          }
+
+          // If handled, stop trying other handlers.
+          if (handled) {
+            break
+          }
+        }
+      } finally {
+        // Reset buffer after processing.
+        buffer = ''
+      }
+    }
+  }
+
+  // Tell each handler to flush once the socket closes.
+  await flushAll()
+
+  if (fd !== undefined) {
+    fs.closeSync(fd)
+  }
+}
 
 main()
 
 process.on('SIGINT', () => {
-  shuttingDown = true
-  // If no modules are reading, exit.
-  if (!reading) {
-    exit()
-  }
-
   console.log('Shutting down after handlers finish...')
-  setInterval(() => {
-    if (!reading) {
-      exit()
-    }
-  }, 1000)
+  if (fd !== undefined) {
+    fs.closeSync(fd)
+    fd = undefined
+  }
 })
