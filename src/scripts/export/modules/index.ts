ae8e99c1420a474a4c59357199b923d624ab1fb1
Noah Saso <noahsaso@gmail.com>
2023-07-09 13:07:59 +0200

    Removed old exporter.

--- src/scripts/export/modules/index.ts
+++
@@ -1,8 +0,0 @@
-import { ModuleExporterMaker } from '../types'
-import { staking } from './staking'
-import { wasm } from './wasm'
-
-export const moduleMakers: Record<string, ModuleExporterMaker> = {
-  staking,
-  wasm,
-}
