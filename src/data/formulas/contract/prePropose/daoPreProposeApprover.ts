606d2cefb9bbdf002719b062134acb9490127fd9
Noah Saso <noahsaso@gmail.com>
2023-11-17 17:38:44 -0800

    Added approver queries.

--- src/data/formulas/contract/prePropose/daoPreProposeApprover.ts
+++ src/data/formulas/contract/prePropose/daoPreProposeApprover.ts
@@ -6,3 +6,19 @@ export const preProposeApprovalContract: ContractFormula<string | undefined> = {
   compute: async ({ contractAddress, get }) =>
     await get(contractAddress, 'pre_propose_approval_contract'),
 }
+
+export const preProposeApprovalIdForApproverProposalId: ContractFormula<
+  number | undefined,
+  { id: string }
+> = {
+  compute: async ({ contractAddress, get, args: { id } }) =>
+    await get(contractAddress, 'proposal_to_pre_propose', Number(id)),
+}
+
+export const approverProposalIdForPreProposeApprovalId: ContractFormula<
+  number | undefined,
+  { id: string }
+> = {
+  compute: async ({ contractAddress, get, args: { id } }) =>
+    await get(contractAddress, 'pre_propose_to_proposal', Number(id)),
+}
