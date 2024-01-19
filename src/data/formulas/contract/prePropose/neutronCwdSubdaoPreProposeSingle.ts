958fdf7a4f283419434ec319bc2ca6f15513dd4b
Noah Saso <noahsaso@gmail.com>
2024-01-19 01:40:19 -0800

    Removed unnecessary formula.

--- src/data/formulas/contract/prePropose/neutronCwdSubdaoPreProposeSingle.ts
+++
@@ -1,6 +0,0 @@
-import { ContractFormula } from '@/core'
-
-export const timelockAddress: ContractFormula<string | undefined> = {
-  compute: async ({ contractAddress, get }) =>
-    await get(contractAddress, 'timelock_contract_address'),
-}
