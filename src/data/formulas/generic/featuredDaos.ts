fa285b75aee3222e83f7534adccee34136dfbf8e
Noah Saso <noahsaso@gmail.com>
2024-01-23 17:01:58 -0800

    Sort featured DAOs across chains.

--- src/data/formulas/generic/featuredDaos.ts
+++ src/data/formulas/generic/featuredDaos.ts
@@ -2,9 +2,18 @@ import { GenericFormula } from '@/core'
 
 import featuredDaosPerChain from './featured_daos.json'
 
-export const featuredDaos: GenericFormula<string[]> = {
+export const featuredDaos: GenericFormula<
+  {
+    address: string
+    order: number
+  }[]
+> = {
   dynamic: true,
   compute: async (env) =>
-    featuredDaosPerChain[env.chainId as keyof typeof featuredDaosPerChain] ||
-    [],
+    featuredDaosPerChain
+      .map((data, index) => ({
+        ...data,
+        order: index + 1,
+      }))
+      .filter(({ chainId }) => chainId === env.chainId),
 }
