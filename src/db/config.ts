14301861612c1d83d0a728783c699dc926407249
Noah Saso <noahsaso@gmail.com>
2024-06-12 18:08:23 -0400

    Revert "add logging when migrating"
    This reverts commit 9075f1dc1c95fa3d3ce2947c2e47971db8764d76.

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,12 +1,5 @@
 import { loadConfig } from '@/core/config'
 const { db } = loadConfig()
 
-// Add logging when running migrations.
-const MIGRATING_DB = process.env.MIGRATING_DB === 'true'
-if (MIGRATING_DB) {
-  db.data.logging = true
-  db.accounts.logging = true
-}
-
 // Export config for .sequelizerc.
 module.exports = db
