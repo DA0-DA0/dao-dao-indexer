1672739867549d5546cfcdaedaf9e4bf1e0ff9fc
Buckram <buckram123@gmail.com>
2024-11-22 04:18:00 +0200

    dodged transformers

--- src/transformers/transformers/index.ts
+++ src/transformers/transformers/index.ts
@@ -8,7 +8,6 @@ import {
   TransformerMaker,
 } from '@/types'
 
-import abstract from './abstract'
 import common from './common'
 import dao from './dao'
 import distribution from './distribution'
@@ -41,7 +40,6 @@ export const getProcessedTransformers = (
       ...staking,
       ...valence,
       ...voting,
-      ...abstract,
 
       // Makers.
       ...transformerMakers.map((maker) => maker(config)),
