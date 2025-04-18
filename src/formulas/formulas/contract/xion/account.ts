9a28e15c214b515d4640fbcedd53a6b47d3dce0d
Noah Saso <6721426+NoahSaso@users.noreply.github.com>
2025-04-18 16:29:48 -0400

    fixed xion/account/authenticators formula

--- src/formulas/formulas/contract/xion/account.ts
+++ src/formulas/formulas/contract/xion/account.ts
@@ -1,33 +1,27 @@
-import { Authenticator } from './types/Account.types'
 import { ContractFormula } from '@/types'
+
+import { Authenticator } from './types/Account.types'
 
 const AccountStorageKeys = {
   AUTHENTICATORS: 'authenticators',
 }
 
-export const authenticators: ContractFormula<Map<number, Authenticator>> = {
+export const authenticators: ContractFormula<Record<number, Authenticator>> = {
   docs: {
     description: 'Get authenticator map for account',
   },
   compute: async (env) => {
     const { contractAddress, getMap } = env
 
-    const authenticatorMap = (await getMap<number, Authenticator>(
-      contractAddress,
-      AccountStorageKeys.AUTHENTICATORS,
-      {
-        keyType: 'number',
-      },
-    )) ?? {}
+    const authenticatorMap =
+      (await getMap<number, Authenticator>(
+        contractAddress,
+        AccountStorageKeys.AUTHENTICATORS,
+        {
+          keyType: 'number',
+        }
+      )) ?? {}
 
-    let responseMap = new Map<number, Authenticator>
-
-    Object.entries(authenticatorMap)
-      .map(([index, authenticator]) => {
-          responseMap.set(+index, authenticator)
-        },
-      )
-
-    return responseMap
+    return authenticatorMap
   },
 }
